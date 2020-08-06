5bc3fd2b5b1f6db6fc0d085a4c3fa4e58ac99998
Alan Baines <abaines@vt.edu>
2020-08-06 01:01:08 -0400

    Update intersection.py
    collect words into matrix and print high-count words

--- python/intersection.py
+++ python/intersection.py
@@ -14,10 +14,27 @@ for file in os.listdir(cwd):
       path = os.path.join(cwd, file)
       dictionaries.append(path)
 
+wordMatrix = {}
 
 for dictPath in dictionaries:
    print(dictPath)
    with open(dictPath, 'r') as file:
       for line in file:
-         print('   ',line.strip())
+         word = line.strip()
+         print('   ',word)
+         if word not in wordMatrix:
+            wordMatrix[word] = 0
+         wordMatrix[word] = 1 + wordMatrix[word]
+
+print(wordMatrix)
+
+fileCount = len(dictionaries)
+
+# print(fileCount)
+
+print()
+
+for word,count in wordMatrix.items():
+   if count >= fileCount:
+      print(word)
 
