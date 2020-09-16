627ee7f1d700388403e263c2fc8c42b10c819cd0
Alan <abaines.vt+github@gmail.com>
2020-09-16 19:36:10 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -4,5 +4,23 @@
 
 document.body.style.backgroundColor = "orange";
 
-console.log("hiya")
+const index = window.location.href
+const home = index.substr(0, index.lastIndexOf("/"))
+const wordsLoc = home + "/words.out"
+
+console.log("home", home)
+console.log("wordsLoc", wordsLoc)
+
+fetch(wordsLoc)
+   .then(
+      response => {
+         console.log(response);
+         console.log(response.body);
+         response.text().then(function (text) {
+            console.log(text);
+         })
+      })
+   .catch(error => {
+      console.log(error);
+   })
 
